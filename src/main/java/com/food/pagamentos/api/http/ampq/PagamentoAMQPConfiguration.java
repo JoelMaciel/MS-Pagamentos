0f6e3ced12ca4c0043d663d138f3d45c85faecd3
Joel Maciel <soldado25461@gmail.com>
2023-05-09 13:57:58 -0300

    fix: pequenas alteraçoes

--- src/main/java/com/food/pagamentos/api/http/ampq/PagamentoAMQPConfiguration.java
+++ src/main/java/com/food/pagamentos/api/http/ampq/PagamentoAMQPConfiguration.java
@@ -1,6 +1,8 @@
 package com.food.pagamentos.api.http.ampq;
 
 import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -10,13 +12,13 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+
 @Configuration
 public class PagamentoAMQPConfiguration {
-
-
     @Bean
-    public RabbitAdmin criaRabbitAdmin(ConnectionFactory connection) {
-        return new RabbitAdmin(connection);
+    public RabbitAdmin criaRabbitAdmin(ConnectionFactory conn) {
+        return new RabbitAdmin(conn);
     }
 
     @Bean
@@ -25,20 +27,21 @@ public class PagamentoAMQPConfiguration {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public Jackson2JsonMessageConverter messageConverter(){
+        return  new Jackson2JsonMessageConverter();
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-                                         Jackson2JsonMessageConverter messageConverter) {
+                                         Jackson2JsonMessageConverter messageConverter){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
-        return rabbitTemplate;
+        return  rabbitTemplate;
     }
 
     @Bean
-    public FanoutExchange fanoutExchange() {
+    public FanoutExchange fanoutExchange(){
         return new FanoutExchange("pagamentos.ex");
     }
+
 }
